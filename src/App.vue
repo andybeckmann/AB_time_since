3ec3f8da12b63ae76fd6eb2d79e68d1fc9765a9a
Andy Beckmann <contact@andybeckmann.com>
2021-09-27 19:00:10 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<TimeSince :date="'09262021'" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	import TimeSince from '@/components/TimeSince.vue'
+	export default {
+		name: 'App',
+		components: {
+			TimeSince
+		}
+	}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TimeSince.vue
@@ -0,0 +1,79 @@
+<template>
+	<div>{{ data }}</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				data: ''
+			}
+		},
+
+		props: {
+			date: String
+		},
+
+		mounted () {
+
+			if (this.date.length > 0) {
+				const 	today = new Date(),
+						todaysMonth = today.getMonth() + 1,
+						todaysYear = today.getFullYear(),
+						todaysDay = today.getDate()
+
+				let difference = 0;
+
+				// If posted today
+				if (this.date == '0' + (todaysMonth).toString() + (todaysDay).toString() + (todaysYear).toString()) {
+					this.data = '0 days ago'
+
+				// If not posted today
+				} else {
+
+					// If post day number is less than todays day number
+					if ((this.date[2] + this.date[3] < todaysDay) && 
+							// And post month is current month
+						(this.date[0] + this.date[1] == todaysMonth) && 
+							// And post year is current year
+						(this.date[4] + this.date[5] + this.date[6] + this.date[7] == todaysYear)) {
+
+						difference = todaysDay - (this.date[2] + this.date[3])
+
+						// Account for pluralisation
+						if (difference > 1) {
+							this.data = difference + ' days ago'
+						} else {
+							this.data = difference + ' day ago'
+						}
+
+					// If post day number is less than todays day number
+					} else if ((this.date[0] + this.date[1] < todaysMonth) &&
+						// And post year is current year
+						(this.date[4] + this.date[5] + this.date[6] + this.date[7] == todaysYear)) {
+						difference = todaysMonth - (this.date[0] + this.date[1])
+
+						// Account for pluralisation
+						if (difference > 1) {
+							this.data = difference + ' months ago'
+						} else {
+							this.data = difference + ' month ago'
+						}
+
+					// If post year is less than current year
+					} else if ((this.date[4] + this.date[5] + this.date[6] + this.date[7]) < todaysYear) {
+						difference = todaysYear - (this.date[4] + this.date[5] + this.date[6] + this.date[7])
+
+						// Account for pluralisation
+						if (difference > 1) {
+							this.data = difference + ' years ago'
+						} else {
+							this.data = difference + ' year ago'
+						}
+					}
+				}
+			}
+		}
+
+	}
+</script>
